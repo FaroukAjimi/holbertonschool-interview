1fa91b689992ce34b2fbb3e69d3c19a443730cd2
FaroukAjimi <benajimi@gmail.com>
2020-08-11 13:55:10 +0100

    retunr0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""Minoop"""
+
+
 def minOperations(n):
+    """miniop"""
     c = 0
     h = 1
     cp = 1
+    if not isinstance(n, int) and n < 2:
+        return 0
     while (h != n):
         if (n % h == 0):
             cp = h
