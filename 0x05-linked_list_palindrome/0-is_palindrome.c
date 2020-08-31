1d487aa477672a1021ed07a4aad9f943eb4e0d71
FaroukAjimi <benajimi@gmail.com>
2020-08-31 21:59:14 +0100

    Soo

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * lent - length of the linked list
+ * @head: head of the linked list
+ * Return: length of the linked list
+ */
+int lent(listint_t *head)
+{
+listint_t *p = head;
+int ct = 1;
+
+while (p->next != NULL)
+{
+ct = ct + 1;
+p = p->next;
+}
+return (ct);
+}
+
+/**
+ * is_palindrome - Check if palindrome
+ * @head: head of the linked list
+ * Return: if 1 not if 0 yes
+ */
+int is_palindrome(listint_t **head)
+{
+listint_t *p = *head;
+int list[lent(*head) / 2];
+int ct = 0;
+int i;
+
+while (ct != lent(*head) / 2)
+{
+list[ct] = p->n;
+p = p->next;
+ct++;
+}
+
+if (lent(*head) % 2 != 0)
+p = p->next;
+for (i = (lent(*head) / 2) - 1 ; i != -1; i--)
+{
+if (p->n != list[i])
+{
+return (0);
+}
+p = p->next;
+}
+return (1);
+}
