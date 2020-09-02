750de964f02d1ac664c46e393d8a24b84a125334
FaroukAjimi <benajimi@gmail.com>
2020-09-02 18:33:56 +0100

    pep8

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -12,7 +12,7 @@ new = [0, 0, 0, 0, 0, 0, 0, 0]
 try:
     for line in sys.stdin:
         s = line.split()
-        su = su + int(s[-1]) 
+        su = su + int(s[-1])
         i = i + 1
         if int(s[-2]) in lis:
             x = lis.index(int(s[-2]))
@@ -22,9 +22,8 @@ try:
             print("File size:", su)
             for x in range(8):
                 print('{}: {}'.format(lis[x], new[x]))
-except:
+except Exception:
     pass
 finally:
     for x in range(8):
         print(lis[x], new[x])
-
