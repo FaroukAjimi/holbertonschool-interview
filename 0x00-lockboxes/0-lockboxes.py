431a87e5d02d4627f32e7d7d14e79e7f28931431
repl.it user <replituser@example.com>
2020-12-17 09:05:35 +0000

    Soo

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -12,9 +12,13 @@ def canUnlockAll(boxes):
     new = [0]
     for i in range(lb):
         rng.append(i)
-    for i in range(lb - 1):
-        for y in range(len(boxes[new[i]])):
-            new.append(boxes[new[i]][y])
+    print(rng)
+    try:
+        for i in range(lb - 1):
+            for y in range(len(boxes[new[i]])):
+                new.append(boxes[new[i]][y])
+    except:
+        return(False)
     new = sorted(new)
     for i in new:
         if i not in uniq:
