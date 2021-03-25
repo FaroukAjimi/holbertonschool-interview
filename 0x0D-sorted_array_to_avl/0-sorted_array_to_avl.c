21884a285e077c923ec55ef18eee657c391d5c87
FaroukAjimi <benajimi@gmail.com>
2021-03-25 22:54:35 +0000

    Soo

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -6,55 +6,51 @@
  * mknode - create new node
  * @array: array
  * @head: head
- * @beg; beginig
- * @lst; last item
- * Return - recursive new list right/left
+ * @beg: beginig
+ * @lst: last item
+ * Return: recursive new list right/left
  */
 avl_t *mknode(int *array, avl_t *head, int beg, int lst)
 {
-  int hlf;
-  avl_t *new;
+int hlf;
+avl_t *new;
 
-  if (beg > lst)
-    return(NULL);
-  hlf = (beg + lst) / 2;
+if (beg > lst)
+return (NULL);
+hlf = (beg + lst) / 2;
+new = malloc(sizeof(avl_t));
+if (!new)
+return (NULL);
 
-  new = malloc(sizeof(avl_t));
-  if (!new)
-    return(NULL);
- 
-  new->n = array[hlf];
-  new->parent = head;
-  new->left = mknode(array, new, beg, hlf - 1);
-  new->right = mknode(array, new, hlf + 1, lst);
-  
-  return(new);
+new->n = array[hlf];
+new->parent = head;
+new->left = mknode(array, new, beg, hlf - 1);
+new->right = mknode(array, new, hlf + 1, lst);
+return (new);
 }
 /**
- * print_array - Prints an array of integers
+ * sorted_array_to_avl - Prints an array of integers
  *
  * @array: The array to be printed
  * @size: Size of the array
+ *Return: avl binary tree
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 avl_t *sav;
 int beg, hlf, lst;
- 
+
 if (!array)
-  return (NULL);
-  
+return (NULL);
 sav = malloc(sizeof(avl_t));
-if (!sav)                                                                                                               return(NULL);
-  
+if (!sav)
+return (NULL);
 beg = 0;
 lst = size - 1;
-hlf = (beg + lst) / 2;  
-
+hlf = (beg + lst) / 2;
 sav->n = array[hlf];
 sav->parent = NULL;
 sav->left = mknode(array, sav, beg, hlf - 1);
 sav->right = mknode(array, sav, hlf + 1, lst);
-
-return(sav);
+return (sav);
 }
