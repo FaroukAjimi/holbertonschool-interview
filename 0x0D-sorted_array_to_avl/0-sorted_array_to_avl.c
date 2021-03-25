7ffe7069ceca4315ff5dfc9567deb538e7817907
FaroukAjimi <benajimi@gmail.com>
2021-03-25 22:25:05 +0000

    Soo

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * mknode - create new node
+ * @array: array
+ * @head: head
+ * @beg; beginig
+ * @lst; last item
+ * Return - recursive new list right/left
+ */
+avl_t *mknode(int *array, avl_t *head, int beg, int lst)
+{
+  int hlf;
+  avl_t *new;
+  if (beg > lst)
+    return(NULL);
+  hlf = (beg + lst) / 2;
+
+  new = malloc(sizeof(avl_t));
+  if (!new)
+    return(NULL);
+ 
+  new->n = array[hlf];
+  new->parent = head;
+  new->left = mknode(array, new, beg, hlf - 1);
+  new->right = mknode(array, new, beg, hlf + 1);
+  
+  return(new);
+}
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Size of the array
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+avl_t *sav;
+int beg, hlf, lst;
+ 
+if (!array)
+  return (NULL);
+  
+sav = malloc(sizeof(avl_t));
+if (!NULL)                                                                                                               return(NULL);
+  
+beg = 0;
+lst = size - 1;
+hlf = (beg + lst) / 2;  
+sav->n = array[hlf];
+sav->parent = NULL;
+sav->left = mknode(array, sav, beg, hlf - 1);
+sav->right = mknode(array, sav, hlf + 1, lst);
+
+return(sav);
+}
---
+++ 0x0D-sorted_array_to_avl/README.md
@@ -0,0 +1,3 @@
+GitHub repository: holbertonschool-interview
+Directory: 0x0D-sorted_array_to_avl
+File: 0-sorted_array_to_avl.c
