e0016d7d0fa344da3bd57bc5040719f66019cde2
FaroukAjimi <benajimi@gmail.com>
2021-04-07 22:03:11 +0000

    So

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,15 +2,15 @@
 const request = require('request');
 const process = require('process');
 const mid = process.argv[2];
-var name = []
 const options = { url: 'https://swapi-api.hbtn.io/api/films/' + mid };
 request.get(options, function (err0, res, body) {
   const jsonbdy = JSON.parse(body);
   const chr = jsonbdy.characters;
   for (let i = 0; i < chr.length; i++) {
-    var n = chr[i];
-    request.get({url : n}, function(err1, res1, body1){
-    var jb = JSON.parse(body1)
-    console.log(jb.name)})
-}
+    const n = chr[i];
+    request.get({ url: n }, function (err1, res1, body1) {
+      const jb = JSON.parse(body1);
+      console.log(jb.name);
+    });
+  }
 });
