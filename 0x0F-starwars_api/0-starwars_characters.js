5b451af743e560c4feaddb80b36301bd18971685
FaroukAjimi <benajimi@gmail.com>
2021-04-08 00:42:35 +0000

    Soo

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,16 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
 const process = require('process');
-const mid = process.argv[2];
-const options = { url: 'https://swapi-api.hbtn.io/api/films/' + mid };
-request.get(options, function (err0, res, body) {
-  const jsonbdy = JSON.parse(body);
-  const chr = jsonbdy.characters;
-  for (let i = 0; i < chr.length; i++) {
-    const n = chr[i];
-    request.get({ url: n }, function (err1, res1, body1) {
-      const jb = JSON.parse(body1);
-      console.log(jb.name);
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+request({ url: url }, (err1, res, body) => {
+  const nid = [];
+  const bjs = JSON.parse(body);
+  bjs.characters.forEach(urls => {
+    const id = urls.split('/')[5];
+    request({ url: urls }, (err2, res, body) => {
+      const bjs1 = JSON.parse(body);
+      nid[id] = bjs1.name;
     });
-  }
+  });
+  setTimeout(function () {
+    bjs.characters.forEach(element => {
+      const id = element.split('/')[5];
+      console.log(nid[id]);
+    });
+  }, 1000);
 });
