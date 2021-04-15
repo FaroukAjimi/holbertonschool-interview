58be65d1667fdcbd35d64fe09c211c411554d023
FaroukAjimi <benajimi@gmail.com>
2021-04-15 03:43:27 +0000

    pep8/doc

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-
+"""Rain interview"""
 
 
 def low(l, r):
+    """lower side"""
     if (r >= l):
         return(l)
     else:
         return(r)
     return(r)
 
+
 def rain(walls):
+    """total rain calculator"""
     total = 0
     lac = 0
     for i in range(1, len(walls) - 1):
