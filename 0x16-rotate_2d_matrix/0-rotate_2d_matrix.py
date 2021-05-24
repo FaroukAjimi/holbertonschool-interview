1fc2d0477a54e86248d090b643a43d2cfd39d3cf
FaroukAjimi <benajimi@gmail.com>
2021-05-24 14:07:35 +0000

    Soo

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Rotation function
+"""
+
+def rotate_2d_matrix(matrix):
+    """matrix - that roatates
+    2d matrix
+    @matrix: the matrixe to rotate"""
+    l = len(matrix)
+    for i in range(l // 2):
+        for y in range(i, l - i - 1):
+            temp = matrix[i][y]
+            matrix[i][y] = matrix[l - 1 - y][i]
+            matrix[l - 1 - y][i] = matrix[l - 1 - i][l - 1 - y]
+            matrix[l - 1 - i][l - 1 - y] = matrix[y][l - 1 - i]
+            matrix[y][l - 1 - i] = temp
---
+++ 0x16-rotate_2d_matrix/README.md
@@ -0,0 +1,1 @@
+Holberton interview rotaion 90
