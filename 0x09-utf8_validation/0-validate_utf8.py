37b8eee3e526a46f69d56b115576ac646cdfea19
FaroukAjimi <benajimi@gmail.com>
2021-06-11 11:22:22 +0000

    SOo

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,29 +1,27 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """
-utf8 vald
+UTF-8 validation
 """
 
 
 def validUTF8(data):
-    """validutf8 - function
-    var : data
-    """
-    b = 0
+    """UTF-8 validation"""
+    byts = 0
     for itm in data:
         r = format(itm, '#010b')[-8:]
-        if b == 0:
+        if byts == 0:
             for bit in r:
                 if bit == '0':
                     break
-                b = b + 1
-            if b == 0:
+                byts += 1
+            if byts == 0:
                 continue
-            if b == 1 or b > 4:
+            if byts == 1 or byts > 4:
                 return False
         else:
-            if not(r[0] == '1' and r[1] == '0'):
+            if not (r[0] == '1' and r[1] == '0'):
                 return False
-        b = b - 1
-    if b == 0:
+        byts = byts - 1
+    if byts == 0:
         return True
     return False
