4d6dc6a42b5ad4b28a634266c849c0c2c4ddafe9
FaroukAjimi <benajimi@gmail.com>
2021-06-21 21:42:26 +0000

    making change

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Function File _ makechange(coins, total)
+"""
+
+def makeChange(coins, total):
+    s = 0
+    t = 0
+    checked = True
+    coins.sort(reverse=True)
+    i = 0
+    while i <= len(coins) - 1: 
+        if (s + coins[i] <= total):
+            s += coins[i]
+            t += 1 
+        else:
+            i += 1
+    if s == total: 
+        return(t)
+    if s == 0:
+        return(0)
+    if s != total:
+        return(-1)
---
+++ 0x19-making_change/README.md
@@ -0,0 +1,3 @@
+Holberton School Interview
+Dir : 0x19 making change
+File : 0-making_change.py 
