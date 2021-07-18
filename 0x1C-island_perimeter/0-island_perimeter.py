09f2ab103e3df8ace8fbbde78b605a968d11c2e3
FaroukAjimi <benajimi@gmail.com>
2021-07-18 13:17:31 +0000

    SO3 G34

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -7,14 +7,12 @@
 def island_perimeter(grid):
     """function perimeter"""
     t = 0
-    multi = 0
     lg = len(grid)
     if lg == 0 or lg > 100:
         return 0
     m = len(grid[0])
     for i in range(lg):
         for j in range(m):
-            multi = 0
             if ((i == 0 or i == lg - 1) and grid[i][j] == 1):
                 return(0)
             if ((j == 0 or j == m - 1) and grid[i][j] == 1):
@@ -22,16 +20,10 @@ def island_perimeter(grid):
             if (grid[i][j] == 1):
                 if (grid[i - 1][j] == 0):
                     t += 1
-                    multi += 1
                 if (grid[i + 1][j] == 0):
                     t += 1
-                    multi += 1
                 if (grid[i][j + 1] == 0):
                     t += 1
-                    multi += 1
                 if (grid[i][j - 1] == 0):
                     t += 1
-                    multi += 1
-            if (multi == 4):
-                return(0)
     return(t)
