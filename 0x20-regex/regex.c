567cab244e54e4db81455d9f39555c19a486a419
FaroukAjimi <benajimi@gmail.com>
2021-08-12 22:21:07 +0000

    commit

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,34 @@
+#include "regex.h"
+/**
+ * regex_match - Good
+ * @str: string to compare
+ * @pattern: comparison
+ * Return: 1/0
+ */
+int regex_match(char const *str, char const *pattern)
+{
+if (*pattern == '\0' && *str == '\0')
+return (1);
+if (*pattern == '\0' || *str == '\0')
+return (0);
+if (*pattern == '.' && *(pattern + 1) == '*' && *(pattern + 2) == '\0')
+return (1);
+if (*(pattern + 1) == '*')
+{
+if (*(pattern) == '.')
+{
+return (regex_match(str, pattern + 2) || regex_match(str + 1, pattern));
+}
+if (*(pattern) != *str)
+return (regex_match(str, pattern + 2));
+if (*(pattern) == *str && *(str + 1) == *str)
+return (regex_match(str + 1, pattern));
+if (*(str + 1) != *str)
+return (regex_match(str + 1, pattern + 2));
+}
+if (*pattern == *str || *pattern == '.')
+{
+return (regex_match(str + 1, pattern + 1));
+}
+return (0);
+}
